3d0e7f1194994c75cbcd0089e97e2ba1dbc719bf
WayneWangPoly <waynewangpoly@gmail.com>
2026-03-16 20:09:52 +1030

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-
-import { Routes, Route, Navigate } from "react-router-dom"
-import DriverSignup from "./pages/DriverSignup"
-import AdminMap from "./pages/AdminMap"
+import { Routes, Route, Navigate } from "react-router-dom";
+import DriverSignup from "./pages/DriverSignup";
+import AdminMap from "./pages/AdminMap";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/driver-signup" />} />
+      <Route path="/" element={<Navigate to="/driver-signup" replace />} />
       <Route path="/driver-signup" element={<DriverSignup />} />
       <Route path="/admin/reward-map" element={<AdminMap />} />
     </Routes>
-  )
+  );
 }
